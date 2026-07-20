2c5be0f01105d66bcb8b48cccbae7038fdcff66b
adejare david <adejaredavid302@gmail.com>
2026-07-20 02:09:06 +0100

    Day 14 Higher or lower game

---
+++ Day_14_Higher_or_Lower_game/game_data.py
@@ -0,0 +1,92 @@
+data = [
+    {
+        "name": "Instagram",
+        "follower_count": 670,
+        "description": "Social media platform",
+        "country": "United States"
+    },
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 630,
+        "description": "Footballer",
+        "country": "Portugal"
+    },
+    {
+        "name": "Lionel Messi",
+        "follower_count": 500,
+        "description": "Footballer",
+        "country": "Argentina"
+    },
+    {
+        "name": "Selena Gomez",
+        "follower_count": 430,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kylie Jenner",
+        "follower_count": 400,
+        "description": "Media personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 395,
+        "description": "Actor and professional wrestler",
+        "country": "United States"
+    },
+    {
+        "name": "Ariana Grande",
+        "follower_count": 380,
+        "description": "Musician and actress",
+        "country": "United States"
+    },
+    {
+        "name": "Kim Kardashian",
+        "follower_count": 364,
+        "description": "Media personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Beyoncé",
+        "follower_count": 320,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "Khloé Kardashian",
+        "follower_count": 310,
+        "description": "Media personality and businesswoman",
+        "country": "United States"
+    },
+    {
+        "name": "Nike",
+        "follower_count": 305,
+        "description": "Sportswear multinational",
+        "country": "United States"
+    },
+    {
+        "name": "Justin Bieber",
+        "follower_count": 293,
+        "description": "Musician",
+        "country": "Canada"
+    },
+    {
+        "name": "Taylor Swift",
+        "follower_count": 284,
+        "description": "Musician",
+        "country": "United States"
+    },
+    {
+        "name": "National Geographic",
+        "follower_count": 283,
+        "description": "Magazine",
+        "country": "United States"
+    },
+    {
+        "name": "Neymar Jr",
+        "follower_count": 222,
+        "description": "Footballer",
+        "country": "Brazil"
+    }
+]
