054b7a4b1109cd4f7a7394a0e2ea7abcc31c2e93
Adejare David <adejaredavid302@gmail.com>
2026-08-11 04:54:38 +0100

    Fix  Day 25 Final Code

--- Day_25_CSV_and_Data_Analysis/main.py
+++ Day_25_CSV_and_Data_Analysis/main.py
@@ -13,7 +13,15 @@ all_states=data["state"].to_list()
 guessed_state=[]
 while len(guessed_state)<len(all_states):
     answer_state=screen.textinput(f"{len(guessed_state)}/50","What another state's name?").title()
-
+    if answer_state == "Exit":
+        not_guessed_state = []
+        for state in guessed_state:
+            if state not in all_states:
+                not_guessed_state.append(state)
+        print(not_guessed_state)
+        new_data = pandas.DataFrame(not_guessed_state)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in all_states:
         guessed_state.append(answer_state)
         dav = turtle.Turtle()
