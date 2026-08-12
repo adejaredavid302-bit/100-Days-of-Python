a67a25b87076089da91d1d590733b5381a4c4120
Adejare David <adejaredavid302@gmail.com>
2026-08-12 03:08:03 +0100

    Update Day 26 code

--- Day_26_List_Comprehension/main.py
+++ Day_26_List_Comprehension/main.py
@@ -1,8 +1,7 @@
 import pandas
 data=pandas.read_csv("nato_phonetic_alphabet.csv")
-print(data.to_dict())
-phonetic_dict={key:value for key,value in data.iterrows()}
+phonetic_dict={row.letter:row.code for (index,row) in data.iterrows()}
 print(phonetic_dict)
-user_word=input("Enter your name: ").lower()
-output_list=[phonetic_dict[user_word] for  letter in user_word ]
+user_word=input("Enter your name: ").upper()
+output_list=[phonetic_dict[letter] for letter in user_word if letter in phonetic_dict ]
 print(output_list)
