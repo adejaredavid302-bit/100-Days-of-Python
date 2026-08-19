0d8c1cb24eebf35b0f66fd9126c7256919085ddb
Adejare David <adejaredavid302@gmail.com>
2026-08-19 04:14:41 +0100

    Day 32 Automated Birthday Wisher project

--- Day_32_Birthday_Wisher/main.py
+++ Day_32_Birthday_Wisher/main.py
@@ -1,1 +1,29 @@
-print("Welcome to Birthday Wisher")
+import smtplib
+import datetime
+import pandas
+import random
+
+
+date = datetime.datetime.now()
+month = date.month
+day = date.day
+today = (month, day)
+
+read_file = pandas.read_csv("birthdays.csv")
+
+birthday_dict = {(row["month"], row["day"]): row.to_dict()for (index, row) in read_file.iterrows()}
+
+if today in birthday_dict:
+    random_letter = random.randint(1, 3)
+    birthday_person = birthday_dict[today]
+    with open(f"./letter_templates/letter_{random_letter}.txt","r") as letter_file:
+        content = letter_file.read()
+
+    birthday_letter = content.replace("[NAME]", birthday_person["name"])
+    my_email = "adejaredavid302@gmail.com"
+    password = "YOUR_APP_PASSWORD"
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(my_email, password)
+        server.sendmail(my_email, birthday_person["email"],f"Subject: Happy Birthday!\n\n{birthday_letter}")
